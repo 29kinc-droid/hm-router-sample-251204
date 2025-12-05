481ab55a9945791280a66a161cdf7273eb380fb6
29kinc <29kinc@gmail.com>
2025-12-05 09:14:32 +0900

    1

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <h1>여기는 홈 입니다.</h1>
+      <p>가장 먼저 보이는 페이지입니다.</p>
+      <Link to="/about">about으로 이동</Link>
+      <Link to="/profile/frontend">profile으로 이동</Link>
+    </>
+  );
+};
+
+export default Home;
